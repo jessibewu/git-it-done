510100ace0cb2daa01a0919d1f4c3656f4ea9c66
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-17 15:51:49 -0400

    added feature search

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -18,6 +19,19 @@ var formSubmitHandler = function(event) {
     nameInputEl.value = '';
   } else {
     alert('Please enter a GitHub username');
+  }
+};
+
+var buttonClickHandler = function(event) {
+
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
+  
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
 
@@ -42,6 +56,20 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert('Unable to connect to GitHub');
     });
+};
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -91,5 +119,9 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+
+
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
+
