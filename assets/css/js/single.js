1b83a962f57179734f6d64efece84b4f1da40284
Jessica Wu <84043367+jessibewu@users.noreply.github.com>
2021-07-17 11:34:24 -0400

    linked both html pages

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -1,9 +1,27 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function(repo) {
 
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -17,12 +35,13 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
       });
   };
   
-  getRepoIssues("facebook/react");
+  getRepoIssues();
 
   var displayIssues = function(issues) {
 
@@ -73,3 +92,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
   };
+
+  getRepoName();
